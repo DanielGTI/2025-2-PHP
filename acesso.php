f764bbf874319fce7ca577e930b14677f858ae13
Daniel Ferreira de Barros Junior <daniel.fb.jr@gmail.com>
2025-11-12 21:43:06 -0300

    acesso fase1

---
+++ acesso.php
@@ -0,0 +1,10 @@
+<?php
+
+    //Capturar os valores do formulário
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    echo "Usuário: " . $username . "<br/>";
+    echo "Senha: " . $password . "<br/>";
+
+?>
---
+++ login.php
@@ -0,0 +1,85 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include 'config/header.php'; ?>
+
+  <body class="login">
+    <div>
+      <a class="hiddenanchor" id="signup"></a>
+      <a class="hiddenanchor" id="signin"></a>
+
+      <div class="login_wrapper">
+        <div class="animate form login_form">
+          <section class="login_content">
+            <form name="form_login" action="acesso.php" method="POST">
+              <h1>Login PHP</h1>
+              <div>
+                <input type="text" name="username" class="form-control" placeholder="Username" required="" />
+              </div>
+              <div>
+                <input type="password" name="password" class="form-control" placeholder="Password" required="" />
+              </div>
+              <div>
+                <input type="submit" class="btn btn-default submit" value="Entrar"/>
+                <a class="reset_pass" href="#">Esqueceu a sua senha?</a
+              </div>
+
+              <div class="clearfix"></div>
+
+              <div class="separator">
+                <p class="change_link">Novo no site?
+                  <a href="#signup" class="to_register"> Criar Conta </a>
+                </p>
+
+                <div class="clearfix"></div>
+                <br />
+
+                <div>
+                  <h1><i class="fa fa-paw"></i> Gentelella PHP!</h1>
+                  <p>©2025 All Rights Reserved. Gentelella PHP</p>
+                </div>
+              </div>
+            </form>
+          </section>
+        </div>
+
+        <div id="register" class="animate form registration_form">
+          <section class="login_content">
+            <form>
+              <h1>Criar Conta</h1>
+              <div>
+                <input type="text" class="form-control" placeholder="Username" required="" />
+              </div>
+              <div>
+                <input type="email" class="form-control" placeholder="Email" required="" />
+              </div>
+              <div>
+                <input type="password" class="form-control" placeholder="Password" required="" />
+              </div>
+              <div>
+                <a class="btn btn-default submit" href="index.html">Enviar</a>
+              </div>
+
+              <div class="clearfix"></div>
+
+              <div class="separator">
+                <p class="change_link">Já é membro ?
+                  <a href="#signin" class="to_register"> Entrar </a>
+                </p>
+
+                <div class="clearfix"></div>
+                <br />
+
+                <div>
+                  <h1><i class="fa fa-paw"></i> Gentelella PHP!</h1>
+                  <p>©2025 All Rights Reserved. Gentelella PHP</p>
+                </div>
+              </div>
+            </form>
+          </section>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
